439acc93889fef25c73e4c8fe418d195aecbd2e0
Martin Greenwood <martin@pixelpudu.com>
2017-06-13 18:58:01 +0100

    fix

--- page-about.php
+++ page-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the What We Offer page.
+ * The template for displaying the about page.
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
